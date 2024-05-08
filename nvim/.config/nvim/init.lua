80712b9e64b4189a4497cff027d99f9c9aa471ff
Jonny Ingall <jonathanaingall@gmail.com>
2024-05-08 15:17:57 +0100

    update theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -736,13 +736,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      require('gruvbox').setup {
+        contrast = 'hard',
+      }
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.o.background = 'light'
+      vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -867,3 +869,11 @@ require('lazy').setup({
 vim.keymap.set('n', '<leader>qj', vim.cmd.cnext, { desc = 'Next entry in quickfix list' })
 vim.keymap.set('n', '<leader>qk', vim.cmd.cprevious, { desc = 'Next entry in quickfix list' })
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open file explorer in current directory' })
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldcolumn = '0'
+vim.opt.foldtext = ''
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = -1
+vim.opt.foldnestmax = 4
