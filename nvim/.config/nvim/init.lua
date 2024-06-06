b8c66c0549f4fe6cb482dd6516b5e6ca46d07991
Jonny Ingall <jonathanaingall@gmail.com>
2024-06-06 20:25:18 +0100

    Updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -736,15 +736,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'neanias/everforest-nvim',
+    'sainnhe/edge',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      require('everforest').setup {
-        background = 'hard',
-      }
+      -- require('everforest').setup {
+      --   background = 'hard',
+      -- }
       -- Load the colorscheme here.
       vim.o.background = 'light'
-      vim.cmd.colorscheme 'everforest'
+      vim.cmd.colorscheme 'edge'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
